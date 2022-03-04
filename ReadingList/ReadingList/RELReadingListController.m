b29ea665e1f4cd0dfeda01c2c8ab51faebd21759
Jonathan Lehr <jonathan@aboutobjects.com>
2022-03-04 17:50:33 -0500

    Added model framework

--- ReadingList/ReadingList/RELReadingListController.m
+++ ReadingList/ReadingList/RELReadingListController.m
@@ -2,6 +2,7 @@
 // See LICENSE.txt for this project's licensing information.
 
 #import "RELReadingListController.h"
+#import <ReadingListModel/ReadingListModel.h>
 
 @implementation RELReadingListController
 
@@ -13,6 +14,23 @@
 
 - (IBAction)cancel:(UIStoryboardSegue *)unwindSegue {
     
+}
+
+// MARK: - UITableViewDataSource methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 100; // FIXME: compute this value!
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Book Cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"Book Cell"];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %@", @(indexPath.row + 1)];
+    
+    return cell;
 }
 
 @end
